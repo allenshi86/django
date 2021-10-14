6982259401491649663fad89b647b9b72bea8b3f
allenshi86 <zhenning.shi@163.com>
2021-10-14 12:21:43 +0800

    update

--- mysite/app01/admin.py
+++ mysite/app01/admin.py
@@ -9,6 +9,7 @@ class AssetsAdmin(admin.ModelAdmin):
     list_filter = ['vendor', 'soft_name', 'dep', 'autho_type']
     search_fields = ['soft_name']
     fields = ['vendor', 'soft_name', 'dep', 'pur_num', 'autho_type', 'pur_date', 'expired_date', 'demander']
+    list_per_page = 30
  #   search_fields =
 
 
